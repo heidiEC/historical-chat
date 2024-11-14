2aeef1ac390f1a4ca960de8896534fff741a8222
heidi Bennett <heidibennett@heidis-MacBook-Pro.local>
2024-11-14 15:40:30 -0800

    Fix image paths for GitHub Pages

--- src/components/CharacterSelect.js
+++ src/components/CharacterSelect.js
@@ -5,79 +5,79 @@ const characters = [
     id: 1,
     name: 'Benjamin Franklin',
     title: 'Founding Father & Polymath',
-    image: '/images/franklin.jpg'
+    image: process.env.PUBLIC_URL + '/images/franklin.jpg'
   },
   {
     id: 2,
     name: 'Martin Luther King Jr',
     title: 'Civil Rights Leader',
-    image: '/images/mlk.jpg'
+    image: process.env.PUBLIC_URL + '/images/mlk.jpg'
   },
   {
     id: 3,
     name: 'Steve Jobs',
     title: 'Tech Visionary',
-    image: '/images/jobs.jpg'
+    image: process.env.PUBLIC_URL + '/images/jobs.jpg'
   },
   {
     id: 4,
     name: 'Leonardo DaVinci',
     title: 'Renaissance Polymath',
-    image: '/images/davinci.jpg'
+    image: process.env.PUBLIC_URL + '/images/davinci.jpg'
   },
   {
     id: 5,
     name: 'J Krishnamurti',
     title: 'Philosophical Teacher',
-    image: '/images/krishnamurti.jpg'
+    image: process.env.PUBLIC_URL + '/images/krishnamurti.jpg'
   },
   {
     id: 6,
     name: 'Stephen Hawking',
     title: 'Theoretical Physicist',
-    image: '/images/hawking.jpg'
+    image: process.env.PUBLIC_URL + '/images/hawking.jpg'
   },
   {
     id: 7,
     name: 'Marie Curie',
     title: 'Pioneer in Radioactivity',
-    image: '/images/curie.jpg'
+    image: process.env.PUBLIC_URL + '/images/curie.jpg'
   },
   {
     id: 8,
     name: 'Albert Einstein',
     title: 'Theoretical Physicist',
-    image: '/images/einstein.jpg'
+    image: process.env.PUBLIC_URL + '/images/einstein.jpg'
   },
   {
     id: 9,
     name: 'Mahatma Gandhi',
     title: 'Non-violent Freedom Fighter',
-    image: '/images/gandhi.jpg'
+    image: process.env.PUBLIC_URL + '/images/gandhi.jpg'
   },
   {
     id: 10,
     name: 'Nikola Tesla',
     title: 'Electrical Engineering Genius',
-    image: '/images/tesla.jpg'
+    image: process.env.PUBLIC_URL + '/images/tesla.jpg'
   },
   {
     id: 11,
     name: 'Maya Angelou',
     title: 'Poet and Civil Rights Activist',
-    image: '/images/angelou.jpg'
+    image: process.env.PUBLIC_URL + '/images/angelou.jpg'
   },
   {
     id: 12,
     name: 'Carl Sagan',
     title: 'Astronomer and Science Communicator',
-    image: '/images/sagan.jpg'
+    image: process.env.PUBLIC_URL + '/images/sagan.jpg'
   },
   {
     id: 13,
     name: 'Princess Diana',
     title: 'Humanitarian & People\'s Princess',
-    image: '/images/diana.jpg'
+    image: process.env.PUBLIC_URL + '/images/diana.jpg'
   }
 ];
 
