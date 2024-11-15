0e2274c060e5d907d8071b31ae9766811925ee53
heidi Bennett <heidibennett@heidis-MacBook-Pro.local>
2024-11-14 16:14:08 -0800

    Update image paths to use relative paths

--- src/components/CharacterSelect.js
+++ src/components/CharacterSelect.js
@@ -7,79 +7,79 @@ const characters = [
     id: 1,
     name: 'Benjamin Franklin',
     title: 'Founding Father & Polymath',
-    image: `${BASE_URL}/images/franklin.jpg`
+    image: './images/franklin.jpg'
   },
   {
     id: 2,
     name: 'Martin Luther King Jr',
     title: 'Civil Rights Leader',
-    image: `${BASE_URL}/images/mlk.jpg`
+    image: './images/mlk.jpg'
   },
   {
     id: 3,
     name: 'Steve Jobs',
     title: 'Tech Visionary',
-    image: `${BASE_URL}/images/jobs.jpg`
+    image: './images/jobs.jpg'
   },
   {
     id: 4,
     name: 'Leonardo DaVinci',
     title: 'Renaissance Polymath',
-    image: `${BASE_URL}/images/davinci.jpg`
+    image: './images/davinci.jpg'
   },
   {
     id: 5,
     name: 'J Krishnamurti',
     title: 'Philosophical Teacher',
-    image: `${BASE_URL}/images/krishnamurti.jpg`
+    image: './images/krishnamurti.jpg'
   },
   {
     id: 6,
     name: 'Stephen Hawking',
     title: 'Theoretical Physicist',
-    image: `${BASE_URL}/images/hawking.jpg`
+    image: './images/hawking.jpg'
   },
   {
     id: 7,
     name: 'Marie Curie',
     title: 'Pioneer in Radioactivity',
-    image: `${BASE_URL}/images/curie.jpg`
+    image: './images/curie.jpg'
   },
   {
     id: 8,
     name: 'Albert Einstein',
     title: 'Theoretical Physicist',
-    image: `${BASE_URL}/images/einstein.jpg`
+    image: './images/einstein.jpg'
   },
   {
     id: 9,
     name: 'Mahatma Gandhi',
     title: 'Non-violent Freedom Fighter',
-    image: `${BASE_URL}/images/gandhi.jpg`
+    image: './images/gandhi.jpg'
   },
   {
     id: 10,
     name: 'Nikola Tesla',
     title: 'Electrical Engineering Genius',
-    image: `${BASE_URL}/images/tesla.jpg`
+    image: './images/tesla.jpg'
   },
   {
     id: 11,
     name: 'Maya Angelou',
     title: 'Poet and Civil Rights Activist',
-    image: `${BASE_URL}/images/angelou.jpg`
+    image: './images/angelou.jpg'
   },
   {
     id: 12,
     name: 'Carl Sagan',
     title: 'Astronomer and Science Communicator',
-    image: `${BASE_URL}/images/sagan.jpg`
+    image: './images/sagan.jpg'
   },
   {
     id: 13,
     name: 'Princess Diana',
     title: 'Humanitarian & People\'s Princess',
-    image: `${BASE_URL}/images/diana.jpg`
+    image: './images/diana.jpg'
   }
 ];
 
