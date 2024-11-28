7c5e1fc1efd1c93b5fd968db1225fbf857976584
Heidi <heidi@everychart.io>
2024-11-28 11:40:27 -0800

    Update App.js
    title change

--- src/App.js
+++ src/App.js
@@ -39,7 +39,7 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <h1>Welcome to the LANE intimate and visionary chats</h1>
+        <h1>The LANE</h1>
       </header>
       
       <div className="nav-controls">
